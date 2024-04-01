7b304eebd4e003d565f2c50305d29c5f8b61393d
Tshering Phuntsho <0223031.cst@rub.edu.bt>
2024-04-01 17:18:59 +0600

    Work in progress

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+import "rsc.io/quote"
+
+func main() {
+    fmt.Println(quote.Go())
+}
---
+++ server.go
@@ -0,0 +1,62 @@
+package main
+
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+    if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(w, "ParseForm() err: %v", err)
+        return
+    }
+    fmt.Fprintf(w, "POST request successful") 
+    name := r.FormValue("name")
+    address := r.FormValue("address")
+    fmt.Fprintf(w, "Name = %s\n", name)
+    fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/hello" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        return
+    }
+
+    fmt.Fprintf(w, "Hello!")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(w, "ParseForm() err: %v", err)
+        return
+    }
+    fmt.Fprintf(w, "successful logged in") 
+    username := r.FormValue("username")
+    password := r.FormValue("password")
+    fmt.Fprintf(w, "username = %s\n", username)
+    fmt.Fprintf(w, "password = %s\n", password)
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fileServer)
+    http.HandleFunc("/form", formHandler)
+    http.HandleFunc("/hello", helloHandler)
+    http.HandleFunc("/login", loginHandler)
+
+
+
+    fmt.Printf("Starting server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
